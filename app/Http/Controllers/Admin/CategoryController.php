e2969d3784d03dbca47c80c9c44a0de08b76ead1
Guilherme Pressutto <gpressutto5@gmail.com>
2017-10-14 19:47:15 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -3,8 +3,8 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Category;
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class CategoryController extends Controller
 {
