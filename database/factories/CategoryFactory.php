f2ea92c36b050d9509e5395957c259601ee2de59
Guilherme Pressutto <gpressutto5@gmail.com>
2017-10-15 16:26:16 -0200

    Each category must be unique

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -4,7 +4,7 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Category::class, function (Faker $faker) {
     return [
-        'name' => $word = $faker->word,
+        'name' => $word = $faker->unique()->word,
         'slug' => $word,
     ];
 });
--- database/migrations/2017_10_14_025114_create_categories_table.php
+++ database/migrations/2017_10_14_025114_create_categories_table.php
@@ -16,8 +16,8 @@ class CreateCategoriesTable extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('slug');
+            $table->string('name')->unique();
+            $table->string('slug')->unique();
             NestedSet::columns($table);
             $table->timestamps();
         });
